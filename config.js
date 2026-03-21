a65b5f57ebead62f09d63cdd0bff197473c7fe6b
Bella63-glitch <bellahbelle63@gmail.com>
2026-03-21 17:27:02 +0300

    Add configuration file with Supabase credentials

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+    S_URL: "https://zchofpncqskvnfrzbcww.supabase.co",
+    S_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpjaG9mcG5jcXNrdm5mcnpiY3d3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxMDI5MTksImV4cCI6MjA4OTQ2MjkxOX0.YJ4_HOCs1cicilDrJch3Ka2ee2BTLHuE9NmvlgTMxfc"
+};
